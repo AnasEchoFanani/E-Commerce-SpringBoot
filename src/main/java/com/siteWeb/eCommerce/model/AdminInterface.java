e3f7c6cfcfe5504a042750f010c78b0c2ea0c925
Anas Fanani <anassfanani123@gmail.com>
2023-09-04 11:20:28 +0100

    Update AdminInterface.java

--- src/main/java/com/siteWeb/eCommerce/model/AdminInterface.java
+++ src/main/java/com/siteWeb/eCommerce/model/AdminInterface.java
@@ -2,6 +2,12 @@ package com.siteWeb.eCommerce.model;
 
 import com.siteWeb.eCommerce.entity.Produit;
 
+import java.util.List;
+
 public interface AdminInterface {
     public void Add_Product(Produit produit);
+    public void Update_Product(Produit produit);
+    public Produit Select_Product_By_Id(int id);
+    public List<Produit> SelectAll_Product();
+    public void Delete_Product(int id);
 }
